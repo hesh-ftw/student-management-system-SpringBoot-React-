b78ce2803e1ea303174d450724d7e46980c61a4c
Chamath Heshan <chamathheshan076@gmail.com>
2024-03-19 18:10:12 +0530

    ADD: REST controllers

--- src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
@@ -1,44 +1,55 @@
-//package com.example.studentmanagementsystem.controller;
-//
-//import com.example.studentmanagementsystem.model.Student;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Controller
-//@RestController
-//public class StudentController {
-//
-//   private List<Student> list= new ArrayList<>(); //declare the student array
-//
-//    @GetMapping("students")
-//   public List<Student> getStudents(){
-//        return list;
-//    }
-//
-//    @GetMapping("student/{id}")
-//    public Student getStudentById(@PathVariable("id") int id){
-//        for(Student student: list){
-//            return student;
-//        }
-//        return null;
-//    }
-//
-//    @PostMapping("students")
-//    public Student addStudent(@RequestBody Student student){
-//        if (studentAlreadyExist(student.getEmail())){
-//                throw studentAlreadyExistException(student.getEmail()+"Already exist");
-//        }
-//        student.setId((long) (list.size()+1));
-//        list.add(student);
-//        return student;
-//    }
-//
-//
-//    private boolean studentAlreadyExist(String email) {
-//        return false;
-//    }
-//
-//}
+package com.example.studentmanagementsystem.controller;
+
+import com.example.studentmanagementsystem.model.Student;
+
+import com.example.studentmanagementsystem.service.StudentServiceInterface;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+// ADD REST CONTROLLERS
+@Controller
+@RestController
+@RequiredArgsConstructor
+
+
+public class StudentController {
+    private final StudentServiceInterface std; //make reference to the studentSrevice Interface
+
+    // get all students
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getStudents(){
+        return new ResponseEntity<>(std.getStudent(), HttpStatus.FOUND);
+    }
+
+    //add new student
+    @PostMapping("/students/add")
+    public Student addStudent(@RequestBody Student student){
+        return std.addStudent(student);
+    }
+
+    //update students
+    @PutMapping("/students/update/{id}")
+    public Student updateStudent(@RequestBody Student student,@PathVariable Long id){
+        return std.updateStudent(student,id);
+    }
+
+    //delete student
+    @DeleteMapping("/students/delete/{id}")
+    public void deleteStudent( @PathVariable Long id){
+        std.deleteStudent(id);
+    }
+
+    //find student by id
+    @GetMapping("/students/{id}")
+    public Student getById(@PathVariable Long id){
+        return std.getStudentById(id);
+    }
+
+}
