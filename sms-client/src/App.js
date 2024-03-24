f1ed4f65a0e1a7ed7ac9a534aafdf4c5ef322d10
Chamath Heshan <chamathheshan076@gmail.com>
2024-03-24 16:05:35 +0530

    second commit

--- sms-client/src/App.js
+++ sms-client/src/App.js
@@ -5,6 +5,27 @@ function App() {
   return (
     <div className="App">
       <h2>welcome to student management system</h2>
+      <table>
+        <section>
+
+          <thead>
+            <tr>
+              <td>first name</td>
+              <td>last name</td>
+              <td>email</td>
+              <td>department</td>
+
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              
+            </tr>
+          </tbody>
+
+        </section>
+      </table>
     </div>
   );
 }
