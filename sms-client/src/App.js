c915a8045927533ebf18f233f86d130c324fe92e
Chamath Heshan <chamathheshan076@gmail.com>
2024-03-27 00:27:58 +0530

    ADD: nav bar

--- sms-client/src/App.js
+++ sms-client/src/App.js
@@ -1,16 +1,33 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import './App.css';
-import Home from './Home';
+import NavBar from "./component/common/NavBar.js";
+import Home from "./Home.js";
 import StudentView from './component/student/StudentView';
-
+import{BrowserRouter as Router, Routers, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h2>welcome to student management system</h2>
-      <Home/>
-      <StudentView/>
+      <h2 >welcome to student management system <p></p></h2>
+
+      <Router>
+         <NavBar/>
+        <Routes>
+          <Route
+              exact 
+              path="/" 
+              element={<Home/>}>
+          </Route>
+
+          <Route
+              exact 
+              path="/view-students" 
+              element={<StudentView/>}>
+          </Route>
+        </Routes>
+      </Router>
+
 
     </div>
   );
