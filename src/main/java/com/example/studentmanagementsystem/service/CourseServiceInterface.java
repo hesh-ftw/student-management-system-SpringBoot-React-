7aa860c7b386961fd0b9614f75f2caf10b87bfdb
Chamath Heshan <chamathheshan076@gmail.com>
2024-06-22 13:11:43 +0530

    removed additional files

--- src/main/java/com/example/studentmanagementsystem/service/CourseServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.studentmanagementsystem.service;
-
-import com.example.studentmanagementsystem.model.Course;
-
-public interface CourseServiceInterface {
-    Course addCourse(Course course);
-
-    Course StudentById(Long id);
-}
