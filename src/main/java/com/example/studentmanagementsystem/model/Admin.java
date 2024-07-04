c7457752774327d8afdeb8e75267a9920b4e7afc
Chamath Heshan <chamathheshan076@gmail.com>
2024-07-05 00:00:23 +0530

    ADD: Admin Entity

---
+++ src/main/java/com/example/studentmanagementsystem/model/Admin.java
@@ -0,0 +1,82 @@
+package com.example.studentmanagementsystem.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long AdminId;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private String encodePassword;
+
+    public Admin(Long adminId, String password, String email, String firstName, String lastName , String encodePassword) {
+        AdminId = adminId;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.encodePassword=encodePassword;
+    }
+
+    public Admin() {
+
+    }
+
+
+    //getters and setters
+
+
+    public Long getAdminId() {
+        return AdminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        AdminId = adminId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
