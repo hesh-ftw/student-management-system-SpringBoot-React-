11e1ebfa5917aecad04ed2ebf1c020e07d0da861
Chamath Heshan <chamathheshan076@gmail.com>
2024-07-06 22:03:49 +0530

    UPDATE : AdminServiceInterface

--- src/main/java/com/example/studentmanagementsystem/service/AdminServiceInterface.java
+++ src/main/java/com/example/studentmanagementsystem/service/AdminServiceInterface.java
@@ -1,8 +1,15 @@
 package com.example.studentmanagementsystem.service;
 
 import com.example.studentmanagementsystem.DTO.AdminDTO;
+import com.example.studentmanagementsystem.DTO.AdminLoginDTO;
+import com.example.studentmanagementsystem.Response.LoginResponse;
 
 
 public interface AdminServiceInterface {
-    String AddAdmin(AdminDTO adminDTO);
+
+    String AdminRegister(AdminDTO adminDTO);
+
+    LoginResponse adminLogin(AdminLoginDTO adminloginDTO);
+
+
 }
