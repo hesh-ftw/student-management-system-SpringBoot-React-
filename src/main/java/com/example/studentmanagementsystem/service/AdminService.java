1ab83a771033ab22fb1378b4e0345809534bcad0
Chamath Heshan <chamathheshan076@gmail.com>
2024-07-06 22:04:38 +0530

    ADD : adminLogin method implementation

--- src/main/java/com/example/studentmanagementsystem/service/AdminService.java
+++ src/main/java/com/example/studentmanagementsystem/service/AdminService.java
@@ -1,6 +1,8 @@
 package com.example.studentmanagementsystem.service;
 
 import com.example.studentmanagementsystem.DTO.AdminDTO;
+import com.example.studentmanagementsystem.DTO.AdminLoginDTO;
+import com.example.studentmanagementsystem.Response.LoginResponse;
 import com.example.studentmanagementsystem.model.Admin;
 import com.example.studentmanagementsystem.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +21,7 @@ public class AdminService implements AdminServiceInterface {
 
 
     @Override
-    public String AddAdmin(AdminDTO adminDTO) {
+    public String AdminRegister(AdminDTO adminDTO) {
         Admin admin = new Admin(
                 adminDTO.getAdminId(),
                 adminDTO.getPassword(),
@@ -28,8 +30,42 @@ public class AdminService implements AdminServiceInterface {
                 adminDTO.getLastName(),
                 this.passwordEncoder.encode(adminDTO.getPassword())
         );
+
         adminRepository.save(admin);
 
         return admin.getEmail();
+
     }
+
+
+    public LoginResponse adminLogin(AdminLoginDTO adminloginDTO){
+
+        // Check if the email is available in the database
+        Admin admin1 = adminRepository.findByEmail(adminloginDTO.getEmail());
+
+        if (admin1 != null) {
+            String password = adminloginDTO.getPassword();
+            String encodePw = admin1.getPassword();
+
+            // Print debug statements
+            System.out.println("Entered Password: " + password);
+            System.out.println("Stored Encoded Password: " + encodePw);
+
+            // Check if the password and encoded password match
+            Boolean isPwRight = passwordEncoder.matches(password, encodePw);
+
+            // Print the result of password matching
+            System.out.println("Is Password Right: " + isPwRight);
+
+            if (isPwRight) {
+                return new LoginResponse("Login successful", true);
+            } else {
+                return new LoginResponse("Password does not match", false);
+            }
+        } else {
+            return new LoginResponse("Email does not match", false);
+        }
+    }
+
+
 }
