c22f6b75d71e0cf16e575572a3852e6bf148880f
Chamath Heshan <chamathheshan076@gmail.com>
2024-07-06 22:05:54 +0530

    ADD : LoginResponse

---
+++ src/main/java/com/example/studentmanagementsystem/Response/LoginResponse.java
@@ -0,0 +1,38 @@
+package com.example.studentmanagementsystem.Response;
+
+public class LoginResponse {
+     String message;
+     boolean status;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String message, boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginMessage{" +
+                "message='" + message + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
