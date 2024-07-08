3b437c5f667c14d457759dbec462ddb18a23c3e2
Chamath Heshan <chamathheshan076@gmail.com>
2024-07-08 14:53:31 +0530

    Update Home.js
    minor changes : home.js

--- sms-client/src/Home.js
+++ sms-client/src/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import image from './image.png';
+import NavBar from './component/common/NavBar';
 
 const Home = () => {
   return (
-    <div className='container'>
+  <div>
+    <NavBar/>
+    <div>
       <div className='home-title'>
-          <h2>Welcome to <br/> 
+          <h2>Welcome to the<br/> 
           Student Management System</h2>
       </div>
       <div className='landing-image'>
@@ -14,10 +17,7 @@ const Home = () => {
       </div>
 
     </div>
-
+  </div>
   )
 }
-
-
-
 export default Home
